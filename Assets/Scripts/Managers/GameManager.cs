484c24cef9ce820726e124c296a2b67e61441617
garinrk <garinrk@gmail.com>
2016-11-29 09:46:50 -0700

    Created game manager and ui manager singletons. Currently goes from start scene > Main menu > main level.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour {
+
+    #region vars
+
+    public static GameManager instance = null;
+
+    #endregion
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        SceneManager.LoadScene(1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
