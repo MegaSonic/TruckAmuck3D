a062e1ab6fe2966e348ccbe7f64de56db5784c49
Adam Kay <adamkay90@gmail.com>
2016-12-15 16:40:19 -0700

    Added things about enemy behaviors

---
+++ Assets/Scripts/Enemy Behaviours/EnemyBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// You should not actually be putting this component on a game object! Put one of the inherited objects on it!!
+/// 
+/// I can't make classes derived from MonoBehaviours virtual or else I would do it. :p
+/// </summary>
+public class EnemyBehaviour : MonoBehaviour {
+
+    public virtual void UpdateEnemy() { Debug.Log("Virtual UpdateEnemy is being called!"); }
+}
